024a166a25a2081e6e27e14ffb24c042ae568b5a
twitchaccoun17-ctrl <twitchaccoun17@gmail.com>
2025-12-10 12:51:13 +0100

    Add files via upload

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+const dev = process.env.NODE_ENV === 'development';
+
+export default {
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : '/NOME-DEL-REPO'   // <-- metti il nome del repo GitHub
+        }
+    }
 };
-
-export default config;
